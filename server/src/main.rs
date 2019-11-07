a453e3e33fb5d8ad67051e218e29b02d88478e18
Alex Krantz <alex@alexkrantz.com>
2019-11-07 08:36:13 -0800

    Add and configure dependencies

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,31 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate diesel;
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+extern crate argonautica;
+extern crate chrono;
+extern crate dotenv;
+extern crate serde;
+extern crate serde_json;
+
+use dotenv::dotenv;
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
+
+#[database("database")]
+pub struct DatabaseConnection(diesel::SqliteConnection);
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+
+    rocket::ignite()
+        .mount("/", routes![])
+        .mount("/static", StaticFiles::from("./static"))
+        .register(catchers![])
+        .attach(DatabaseConnection::fairing())
+        .attach(Template::fairing())
+        .launch();
 }
