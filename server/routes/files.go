e645eeed56ef19d3ae1d0f7e30020a0db26be5b9
Alex Krantz <alex@alexkrantz.com>
2019-12-04 20:57:21 -0800

    Add file management

--- server/routes/files.go
+++ server/routes/files.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"encoding/json"
 	"github.com/akrantz01/bookpi/server/responses"
 	"github.com/gorilla/mux"
 	"io"
@@ -22,7 +23,7 @@ func Files(filesDirectory string, router *mux.Router) {
 func fileRouter(filesDirectory string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// Assemble full path
-		p := path.Join(filesDirectory, r.Header.Get("X-BPI-Username"), strings.TrimPrefix(r.RequestURI, "/api/files"))
+		p := path.Join(filesDirectory, r.Header.Get("X-BPI-Username"), strings.TrimPrefix(r.URL.Path, "/api/files"))
 
 		switch r.Method {
 		case http.MethodGet:
@@ -32,7 +33,7 @@ func fileRouter(filesDirectory string) func(w http.ResponseWriter, r *http.Reque
 			createFile(w, r, p)
 
 		case http.MethodPut:
-			updateFile(w, r, p)
+			updateFile(w, r, p, filesDirectory)
 
 		case http.MethodDelete:
 			deleteFile(w, r, p)
@@ -91,12 +92,14 @@ func listFiles(w http.ResponseWriter, r *http.Request, path string) {
 		children = []map[string]interface{}{}
 	}
 
+	rawPath := filepath.Clean(strings.TrimPrefix(r.RequestURI, "/api/files"))
+
 	responses.SuccessWithData(w, map[string]interface{}{
 		"name": info.Name(),
 		"size": info.Size(),
 		"last_modified": info.ModTime().Unix(),
 		"directory": info.IsDir(),
-		"root": filepath.Clean(strings.TrimPrefix(r.RequestURI, "/api/files")) == "/",
+		"root": rawPath == "/" || rawPath == ".",
 		"children": children,
 	})
 }
@@ -109,20 +112,38 @@ func createFile(w http.ResponseWriter, r *http.Request, path string) {
 		return
 	}
 
+	// Check if creating directory
+	directory := r.URL.Query().Get("directory") != ""
+
 	// Get file statistics
 	info, err := os.Stat(path)
-	if os.IsNotExist(err) {
-		responses.Error(w, http.StatusNotFound, "specified file/directory does not exist")
+	if os.IsNotExist(err) && !directory {
+		responses.Error(w, http.StatusNotFound, "specified directory does not exist")
 		return
-	} else if err != nil {
+	} else if !os.IsNotExist(err) && directory {
+		responses.Error(w, http.StatusConflict, "specified directory already exists")
+		return
+	} else if err != nil && ((os.IsExist(err) && directory) || (os.IsNotExist(err) && !directory)) {
 		log.Printf("ERROR: failed to stat file: %v\n", err)
 		responses.Error(w, http.StatusInternalServerError, "failed to stat file")
 		return
 	}
 
 	// Ensure not uploading to file
-	if !info.IsDir() {
+	if !directory && !info.IsDir() {
 		responses.Error(w, http.StatusBadRequest, "cannot upload to file")
+		return
+	}
+
+	// Check if uploading directory
+	if directory {
+		if err := os.Mkdir(path, os.ModeDir|0755); err != nil {
+			log.Printf("ERROR: fialed to create new directory under user: %v\n", err)
+			responses.Error(w, http.StatusInternalServerError, "failed to create directory")
+			return
+		}
+
+		responses.Success(w)
 		return
 	}
 
@@ -183,11 +204,113 @@ func createFile(w http.ResponseWriter, r *http.Request, path string) {
 }
 
 // Change a file's name on disk
-func updateFile(w http.ResponseWriter, r *http.Request, path string) {
+func updateFile(w http.ResponseWriter, r *http.Request, path, filesDirectory string) {
+	// Don't allow changes to user root
+	rawPath := filepath.Clean(strings.TrimPrefix(r.RequestURI, "/api/files"))
+	if rawPath == "." || rawPath == "/" {
+		responses.Error(w, http.StatusForbidden, "not allowed to move user root")
+		return
+	}
 
+	// Validate initial request on headers and body existence
+	if r.Header.Get("Content-Type") != "application/json" {
+		responses.Error(w, http.StatusBadRequest, "header 'Content-Type' must be 'application/json'")
+		return
+	} else if r.Body == nil {
+		responses.Error(w, http.StatusBadRequest, "request body must be present")
+		return
+	}
+
+	// Parse and validate body fields
+	var body struct{
+		Filename string `json:"filename"`
+		Path string `json:"path"`
+	}
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		responses.Error(w, http.StatusBadRequest, "invalid json format for request body")
+		return
+	} else if body.Path != "" && body.Filename != "" {
+		responses.Error(w, http.StatusBadRequest, "cannot change path and filename at the same time")
+		return
+	}
+
+	// Get file statistics and ensure exists
+	_, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		responses.Error(w, http.StatusNotFound, "specified file/directory does not exist")
+		return
+	} else if err != nil {
+		log.Printf("ERROR: failed to stat file: %v\n", err)
+		responses.Error(w, http.StatusInternalServerError, "failed to stat file")
+		return
+	}
+
+	// Rename file if passed
+	if body.Filename != "" {
+		newName := filepath.Base(filepath.Clean(body.Filename))
+		directory := filepath.Dir(path)
+
+		// Rename file
+		if err := os.Rename(path, directory+"/"+newName); err != nil {
+			log.Printf("ERROR: failed to rename file: %v\n", err)
+			responses.Error(w, http.StatusInternalServerError, "failed to rename file")
+			return
+		}
+	}
+
+	// Move file if passed
+	if body.Path != "" {
+		newPath := filepath.Join(filesDirectory, r.Header.Get("X-BPI-Username"), strings.Replace(body.Path, "../", "", -1))
+		filename := filepath.Base(path)
+
+		// Ensure new path exists
+		if _, err := os.Stat(newPath); os.IsNotExist(err) {
+			responses.Error(w, http.StatusBadRequest, "specified path does not exist")
+			return
+		}
+
+		// Move file
+		if err := os.Rename(path, newPath+"/"+filename); err != nil {
+			log.Printf("ERROR: failed to move file to specified directory: %v\n", err)
+			responses.Error(w, http.StatusInternalServerError, "failed to move file")
+			return
+		}
+	}
+
+	responses.Success(w)
 }
 
 // Delete a file
-func deleteFile(w http.ResponseWriter, r *http.Request, path string) {
+func deleteFile(w http.ResponseWriter, _ *http.Request, path string) {
+	// Ensure file exists
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		responses.Error(w, http.StatusNotFound, "specified file/directory does not exist")
+		return
+	} else if err != nil {
+		log.Printf("ERROR: failed to stat file: %v\n", err)
+		responses.Error(w, http.StatusInternalServerError, "failed to stat file")
+		return
+	}
 
+	// Remove all under if directory
+	if info.IsDir() {
+		if err := os.RemoveAll(path); err != nil {
+			log.Printf("ERROR: failed to delete directory: %v\n", err)
+			responses.Error(w, http.StatusInternalServerError, "failed to delete directory")
+			return
+		}
+
+		responses.Success(w)
+		return
+	}
+
+	// Remove file
+	if err := os.Remove(path); err != nil {
+		log.Printf("ERROR: failed to delete file: %v\n", err)
+		responses.Error(w, http.StatusInternalServerError, "failed to remove file")
+		return
+	}
+
+	responses.Success(w)
 }
