2ce7c2569f7289ab9dfeb367c868e7e2ce342f41
Alex Krantz <alex@alexkrantz.com>
2019-12-07 23:23:49 -0800

    Add toast notifications

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 import Router from './router';
 
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import {HashRouter, Switch, Route} from 'react-router-dom';
+import { ToastContainer, Flip } from "react-toastify";
 
 import Header from "./components/header";
 import Footer from "./components/footer";
 
 import Home from './pages/home';
+import SignIn from './pages/sign-in';
 
 class Router extends Component {
     constructor(props) {
@@ -21,10 +23,12 @@ class Router extends Component {
     render() {
         return (
             <HashRouter>
+                <ToastContainer position="top-right" autoClose={4000} newestOnTop closeOnClick pauseOnHover draggable transition={Flip}/>
                 <Header loggedIn={this.state.loggedIn} toggleLogin={this.toggleLogin.bind(this)}/>
                 <main role="main" className="flex-shrink-0" style={{ marginTop: "40px" }}>
                     <Switch>
                         <Route path="/" exact><Home/></Route>
+                        <Route path="/sign-in" exact><SignIn/></Route>
                     </Switch>
                 </main>
                 <Footer/>
