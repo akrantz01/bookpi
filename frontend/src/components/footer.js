5a79974789ad43b9d1678c108c737eaf61723a0b
Alex Krantz <alex@krantz.dev>
2020-04-30 16:20:14 -0700

    Update year and fix footer stickiness

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -7,11 +7,11 @@ class Footer extends Component {
         if (this.props.location.pathname === "/sign-in" || this.props.location.pathname === "/sign-up") return (<></>);
 
         return (
-            <footer style={{ position: "absolute", bottom: 0, left: 0, width: "100%", lineHeight: "45px", backgroundColor: "#f5f5f5"}}>
+            <footer style={{ position: "fixed", bottom: 0, left: 0, width: "100%", lineHeight: "45px", backgroundColor: "#f5f5f5"}}>
                 <div className="container">
                     <div className="row justify-content-between">
                         <div className="col-md">
-                            <p className="text-muted" style={{ marginBottom: 0 }}>BookPi &copy; <a style={{ color: "darkgray" }} href="https://krantz.dev" target="_blank" rel="noopener noreferrer">Alexander Krantz</a> 2019</p>
+                            <p className="text-muted" style={{ marginBottom: 0 }}>BookPi &copy; <a style={{ color: "darkgray" }} href="https://krantz.dev" target="_blank" rel="noopener noreferrer">Alexander Krantz</a> 2020</p>
                         </div>
                     </div>
                 </div>
