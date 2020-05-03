6465fe6519ac4aa76f8ea8b22a6dc9574767c7fa
Alex Krantz <alex@krantz.dev>
2020-05-03 00:59:12 -0700

    Fix inability to bind to port 80

--- install.sh
+++ install.sh
@@ -43,6 +43,7 @@ mv bookpi-*-server /usr/local/bin/bookpi-server
 mv bookpi-*-display /usr/local/bin/bookpi-display
 mv bookpi.sh /usr/local/bin/bookpi
 chmod +x /usr/local/bin/bookpi /usr/local/bin/bookpi-server /usr/local/bin/bookpi-display
+setcap "cap_net_bind_service=+ep" /usr/local/bin/bookpi-server
 
 # Install systemd units
 echo "Installing services as systemd units..."
